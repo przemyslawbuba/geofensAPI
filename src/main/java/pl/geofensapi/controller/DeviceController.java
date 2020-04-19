21d9462130176a2a5a617e60d7b3a45e8036df14
Przemyslaw Buba <233085@edu.p.lodz.pl>
2020-04-19 19:57:26 +0200

    Dodanie swaggera

--- src/main/java/pl/geofensapi/controller/DeviceController.java
+++ src/main/java/pl/geofensapi/controller/DeviceController.java
@@ -1,19 +1,29 @@
 package pl.geofensapi.controller;
 
+
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 import pl.geofensapi.entity.Device;
 import pl.geofensapi.entity.request.AddDeviceRequest;
 import pl.geofensapi.repository.DeviceRepository;
 
 import java.util.List;
 
+
 @RestController
+
 public class DeviceController {
 
+    @Autowired
     private DeviceRepository deviceRepository;
 
     @Autowired
@@ -21,16 +31,28 @@ public class DeviceController {
         this.deviceRepository = deviceRepository;
     }
 
-    @RequestMapping(value = "/device", method = RequestMethod.GET)
+    @RequestMapping(value = "/list", method= RequestMethod.GET)
+    @ApiOperation(value = "Device list")
     public List<Device> findAllDevice() {
         return deviceRepository.findAll();
     }
 
-    @RequestMapping(value = "/device", method = RequestMethod.POST)
+    @PostMapping("/device")
+    @ApiOperation(value = "Add a device")
     public void addDevice(@RequestBody AddDeviceRequest addDeviceRequest){
         Device device = new Device();
         device.setName(addDeviceRequest.getName());
         deviceRepository.save(device);
     }
+
+    @ApiOperation(value = "Delete a device")
+    @RequestMapping(value="/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    public ResponseEntity delete(@PathVariable(value = "id") Long id){
+        Device device = new Device();
+        device = deviceRepository.getOne(id);
+        deviceRepository.delete(device);
+        return new ResponseEntity("Product deleted successfully", HttpStatus.OK);
+    }
+
 
 }
--- src/main/java/pl/geofensapi/entity/Device.java
+++ src/main/java/pl/geofensapi/entity/Device.java
@@ -1,10 +1,15 @@
 package pl.geofensapi.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@ApiModel(description = "All details about the device. ")
 public class Device {
 
     @Id
@@ -12,7 +17,7 @@ public class Device {
     private long id;
     private String name;
 
-
+    @ApiModelProperty(notes = "The database generated device ID")
     public long getId() {
         return id;
     }
